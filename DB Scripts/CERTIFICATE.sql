7ce689612fb417b8243b94eb015fa16ddfbeb22e
Trương Ý Nhi <20521720@gm.uit.edu.vn>
2022-04-07 16:12:23 +0700

    Update CERTIFICATE.sql

--- DB Scripts/CERTIFICATE.sql
+++ DB Scripts/CERTIFICATE.sql
@@ -3,6 +3,17 @@
 --Create a table struct without constraint
 --<Attribute name> <Data type>
 
+create table CERTIFICATE
+(
+--PersonalID FK references PERSON(ID), personal identifier consisting of 12 digits
+PersonalID varchar2(12),
+--The number of injected dose
+Dose number(2),
+--The certificate of vaccination against covid,the value 0,1,2 respectively represents a red card,yellow card, green card 
+CertType number(1),
+--Notes on certificate information
+Note varchar2(2000),
+)
 
 
 /*	CONSTRAINT	*/
