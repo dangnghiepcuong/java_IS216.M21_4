8b0e68be92481c2c99169dd22b6aae7a927a700f
TNQT14 <87488896+TNQT14@users.noreply.github.com>
2022-04-08 04:08:50 +0700

    Update ORGANIZATION.sql

--- DB Scripts/ORGANIZATION.sql
+++ DB Scripts/ORGANIZATION.sql
@@ -2,32 +2,45 @@
 /*          TABLE: ORGANIZATION          */
 --Create a table struct without constraint
 --<Attribute name> <Data type>
-
+create table ORGANIZATION
+(
+    ID varchar2(16),
+    Name varchar2(100),
+    Province varchar2(50),
+    District varchar2(50),
+    Town varchar2(50),
+    Street varchar2(100),
+    Note varchar2(2000)
+);
 
 
 /*	CONSTRAINT	*/
 --Primary Key
-
-
+alter table ORGANIZATION 
+add constraint PK_ORGANIZATION primary key (ID);
 
 --Foreign Key
-
+alter table ORGANIZATION 
+add constraint FK_ORG_ACC foreign key (ID) references ACCOUNT(OrgID);
 
 
 --Check
+    --Check name is not null
+alter table ORGANIZATION
+add constraint CHK_NAME check (Name is not null);
 
-
-
+alter table ORGANIZATION
+add constraint CHK_PROVINCE check (Province is not null);
 /*	TRIGGERS	*/
 
 
 
 /*	STORED PROCEDURES	*/
-
+--
 
 
 /*	STORED FUNCTIONS	*/
-
+--
 
 
 
