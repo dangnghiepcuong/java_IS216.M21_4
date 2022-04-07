0030576682a0d4ed44063e38da8568a303768010
TNQT14 <87488896+TNQT14@users.noreply.github.com>
2022-04-07 15:25:02 +0700

    Update ANNOUNCEMENT.sql

--- DB Scripts/ANNOUNCEMENT.sql
+++ DB Scripts/ANNOUNCEMENT.sql
@@ -2,13 +2,22 @@
 /*          TABLE: ANNOUNCEMENT          */
 --Create a table struct without constraint
 --<Attribute name> <Data type>
+create table ANNOUNCEMENT
+(
+    ID varchar2(50),
+    OrgID varchar2(16),
+    Title varchar2(200),
+    Content varchar2(4000),
+    PublishDate date,
+    Image blob,
+    Note varchar2(2000)
+);
 
-
+Drop table ANNOUNCEMENT;
 
 /*	CONSTRAINT	*/
 --Primary Key
-
-
+alter table ANNOUNCEMENT add constraint PK_ANNOUNCEMENT primary key (ID,OrgID);
 
 --Foreign Key
 
