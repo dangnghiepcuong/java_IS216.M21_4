4cf73d202beb5ea460744590a105e056a207c9b0
Đặng Nghiệp Cường <20520418@gm.uit.edu.vn>
2022-04-07 20:55:16 +0700

    Update REGISTER.sql

--- DB Scripts/REGISTER.sql
+++ DB Scripts/REGISTER.sql
@@ -57,10 +57,13 @@ begin
 	
 
 /*	STORED PROCEDURES	*/
+--Put in PersonalID, SchedId, TimeRegistered. Calculate the number order (NO) of registion
 create or replace procedure REG_SIGNED_NO (par_PersonalID PERSON.ID%type, par_SchedID SCHEDULE.ID%type, par_TimeReg REGISTER.Time%type)
 as
+	--par_ to get the present value of the registered time limit number
 	LimitReg REGISTER.NO%type;
 begin
+	--from the registered time, select its limit number from SCHEDULE
 	select LimitReg,
 		case par_TimeReg when 0 then LimitDay
 			when 1 then LimitNoon
@@ -69,12 +72,16 @@ begin
 			raise_application_error(100001, 'Time registion not found!')
 		end
 	from SCHEDULE
-	where SCHEDULE.ID = par_SchedID;
+	where SCHEDULE.ID = par_SchedID
+	and SCHEDULE.PersonalID = par_PersonalID;	
 	
+	--After getting the Limit number
+	--Set the NO for registion by Limit number + 1
 	update REGISTER
 	set NO = LimitReg + 1;
 	where REGISTER.PersonalID = par_PersonalID
 	and REGISTER.SchedID = par_SchedID;
+
 end REG_SIGNED_NO;
 
 /*	STORED FUNCTIONS	*/
