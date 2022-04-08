be5f14ab07b8da9a9c5573e46f1010eb4fd3edb4
TNQT14 <87488896+TNQT14@users.noreply.github.com>
2022-04-09 06:42:09 +0700

    Update ORGANIZATION.sql

--- DB Scripts/ORGANIZATION.sql
+++ DB Scripts/ORGANIZATION.sql
@@ -50,49 +50,112 @@ add constraint CHK_PROVINCE check (Province is not null);
 
 /*	STORED PROCEDURES	*/
 --Insert
-create or replace procedure ORG_INSERT_RECORD (org_ID ORGANIZATION.ID%type,
-                                             org_Name ORGANIZATION.Name%type,
-                                             org_Province ORGANIZATION.Province%type,
-                                             org_District ORGANIZATION.District%type,
-                                             org_Town ORGANIZATION.Town%type,
-                                             org_Street ORGANIZATION.Street%type)                                           
+create or replace procedure ORG_INSERT_RECORD (par_ID ORGANIZATION.ID%type,
+                                             par_Name ORGANIZATION.Name%type,
+                                             par_Province ORGANIZATION.Province%type,
+                                             par_District ORGANIZATION.District%type,
+                                             par_Town ORGANIZATION.Town%type,
+                                             par_Street ORGANIZATION.Street%type)                                           
 as 
 begin
-    --insert new ORGANIZATION
+    --insert new record ORGANIZATION
     
 	insert into ORGANIZATION(ID, Name, Province, District, Town, Street, Note) 
-    values (org_ID, org_Name, org_Province, org_District, org_Town, org_Street,
+    values (par_ID, par_Name, par_Province, par_District, par_Town, par_Street,
     NULL);
+    
 end;
 
 --Delete
-create or replace procedure ORG_DELETE_RECORD (Org_ID ORGANIZATION.ID%type)
+create or replace procedure ORG_DELETE_RECORD (par_ID ORGANIZATION.ID%type)
 as
 begin
+    ----delete record ORGANIZATION
     delete 
     from ORGANIZATION
-    where ID=Org_ID;
+    where ID=par_ID;
 end;
 
 --Update
-create or replace procedure ORG_UPDATE_RECORD (Org_ID ORGANIZATION.ID%type,
-                                                org_Name ORGANIZATION.Name%type,
-                                                org_Province ORGANIZATION.Province%type)
+create or replace procedure ORG_UPDATE_RECORD (par_ID ORGANIZATION.ID%type,
+                                                par_Name ORGANIZATION.Name%type,
+                                                par_Province ORGANIZATION.Province%type,
+                                                par_District ORGANIZATION.District%type,
+                                                par_Town ORGANIZATION.Town%type,
+                                                par_Street ORGANIZATION.Street%type,
+                                                par_Note ORGANIZATION.Note%type)
 as
 begin
+    --Update record ORGANIZATION
+
     update ORGANIZATION
-    set Name=org_Name,
-        Province=org_Province
-    where ID=Org_ID;
+    set Name=par_Name,
+        Province=par_Province,
+        District=par_District,
+        Town=par_Town,
+        Street=par_Street,
+        Note=par_Note
+    where ID=par_ID;
 end;
 
-EXEC ORG_UPDATE_RECORD(5,'h','ha')
+
 
 --Annouce
 
 
 /*	STORED FUNCTIONS	*/
---
+
+-- Count the number of schedules were hold by the ORG from StartDate to EndDate
+create or replace function ORG_COUNT_SCHED (par_ID ORGANIZATION.ID%type,
+                                            par_StatDate date,
+                                            par_EndDate date)
+return number is 
+--return value of count schedule
+    Count_Sched int;
+begin
+    --assign value 0 for Count_Sched
+    Count_Sched :=0; 
+   --check start date must be less than or equal to end date
+    if (par_StatDate<=par_EndDate) then
+    begin
+        --from the schedule, count OrgID
+        select count(OrtgID) into Count_Sched
+        from SCHEDULE SCHED
+        where SCHED.OnDate >= par_StatDate and SCHED.OnDate <= par_EndDate;
+    end;
+    else 
+        DBMS_OUTPUT.PUT_LINE('start date must be less than or equal to end date');
+    end if;
+end;
+
+--Count the number of injections have been done by the ORG from StartDate to EndDate
+create or replace function ORG_COUNT_INJ (par_ID ORGANIZATION.ID%type,
+                                            par_StatDate date,
+                                            par_EndDate date)
+return number is 
+--return value of count schedule
+    Count_Inj int;
+    
+begin
+    if (par_StatDate<=par_EndDate) then
+    begin
+    ---count the number of injections have been done by the ORG from StartDate 
+    --to EndDate
+        select count(status) into Count_Inj, sched.ID
+        from SCHEDULE sched join REGISTER reg on reg.SCHEDID=sched.ID
+        where reg.Status=2 and SCHED.OnDate >= par_StatDate
+              and SCHED.OnDate <= par_EndDate
+        group by sched.ID;
+        
+        if(Count_Inj = 0) then
+            dbms_output.put_line('no previous injections');
+        end if;
+    end;
+    else 
+        DBMS_OUTPUT.PUT_LINE('start date must be less than or equal to end date');
+    end if;
+    
+end;
 
 
 
