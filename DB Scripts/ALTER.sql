145e26d07234a0078383a8b9ec411cc183bc65fb
Đặng Nghiệp Cường <20520418@gm.uit.edu.vn>
2022-04-10 22:29:28 +0700

    .

---
+++ DB Scripts/ALTER.sql
@@ -0,0 +1,23 @@
+/*	ALTER DATABASE COMMAND	*/
+
+
+/*	20520418	- Dang Nghiep Cuong		
+Written: 
+Executed: 
+*/
+
+/*	20521252 -  Le Hoang Duyen		
+Written:
+Executed:
+*/
+
+/*	20521720 - Truong Y Nhi		
+Written: 
+Executed:
+*/
+
+/*	20521890 - Truong Nguyen Quang Thai	
+Written: 
+Executed:
+*/
+
