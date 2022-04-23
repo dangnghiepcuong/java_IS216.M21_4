9419244370f755cfce6b587ac8ff159fdd396a9e
Trương Ý Nhi <20521720@gm.uit.edu.vn>
2022-04-23 09:30:03 +0700

    CERT, HEAL, drop

--- DB Scripts/HEALTH.sql
+++ DB Scripts/HEALTH.sql
@@ -25,9 +25,22 @@ add constraint FK_HEAL_PERSON foreign key (PersonalID) references PERSON(ID)
 --Check
 
 
-
 /*	TRIGGERS	*/
+---check Filleddate=SYSDATE
 
+create or replace trigger HEALTH_FilledDate
+after insert on HEALTH
+for each row
+begin
+if((Extract(Year From : new.FilledDate) = Extract(Year From SYSDATE))
+and (Extract(Month From : new.FilledDate) = Extract(Year From SYSDATE))
+and (Extract(Year From : new.FilledDate) = Extract(Year From SYSDATE))
+then 
+DBMS_OUTPUT.PUT_LINE('Successfully added filled date');
+else
+RAISE_APPLICATION_ERROR(-10000,'Filled date must be according to SYSDATE!);
+end if;
+end;
 
 
 /*	STORED PROCEDURES	*/
