c4d8681cbca8192d40cd0c0f114ceacd9afbfc1a
Nhi230600 <20521720@gm.uit.edu.vn>
2022-04-25 10:39:46 +0700

    health

--- SQL Scripts/HEALTH.sql
+++ SQL Scripts/HEALTH.sql
@@ -1,15 +1,10 @@
---
---20521720 - Trương Ý Nhi
 /*          TABLE: HEALTH          */
 --Create a table struct without constraint
 --<Attribute name> <Data type>
 
 create table HEALTH
 (
-<<<<<<< Updated upstream
-=======
       ID int,
->>>>>>> Stashed changes
 	PersonalID varchar2(12),
 	FilledDate date,
 	Healths varchar2(20),
@@ -19,16 +14,14 @@ create table HEALTH
 /*	CONSTRAINT	*/
 --Primary Key
 alter table HEALTH
-add constraint PK_HEAL primary key (PersonalID)
-
+add constraint PK_HEAL primary key (PersonalID, ID);
 
 --Foreign Key
 alter table HEALTH
-add constraint FK_HEAL_PERSON foreign key (PersonalID) references PERSON(ID)
+add constraint FK_HEAL_PERSON foreign key (PersonalID) references PERSON(ID);
 
 
 --Check
-
 
 /*	TRIGGERS	*/
 ---check Filleddate=SYSDATE
@@ -37,27 +30,31 @@ create or replace trigger HEALTH_FilledDate
 after insert or update on HEALTH
 for each row
 begin
-<<<<<<< Updated upstream
-if((Extract(Year From : new.FilledDate) = Extract(Year From SYSDATE))
-and (Extract(Month From : new.FilledDate) = Extract(Year From SYSDATE))
-and (Extract(Year From : new.FilledDate) = Extract(Year From SYSDATE))
-then 
-DBMS_OUTPUT.PUT_LINE('Successfully added filled date');
-else
-RAISE_APPLICATION_ERROR(-10000,'Filled date must be according to SYSDATE!);
-=======
     if((Extract(Day From : new.FilledDate) != Extract(Day From SYSDATE))
     and (Extract(Month From : new.FilledDate) != Extract(Month From SYSDATE))
     and (Extract(Year From : new.FilledDate) != Extract(Year From SYSDATE))
     then 
         RAISE_APPLICATION_ERROR(-10000,'Filled date must be according to SYSDATE!);
->>>>>>> Stashed changes
 end if;
 end HEALTH_FilledDate;
 
 
 /*	STORED PROCEDURES	*/
+--insert
 
+create or replace procedure HEAL_INSERT_RECORD
+(par_ID HEALTH.ID%type,
+par_PersonalID PERSON.ID%type, 
+par_FilledDate HEALTH.FilledDate%type, 
+par_Healths HEALTH.Healths%type, 
+par_Note HEALTH.Note%type DEFAULT NULL)
+as
+
+begin
+    insert into HEALTH(ID, PersonalID, FilledDate, Healths, Note)
+    values(par_ID, par_PersonalID, par_FilledDate, par_Healths, par_Note);
+    commit;
+end HEAL_INSERT_RECORD;
 
 
 /*	STORED FUNCTIONS	*/
