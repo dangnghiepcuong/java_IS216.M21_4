f6972ff9e1323aa0f884aa73a5c5671d4c646942
Nhi230600 <20521720@gm.uit.edu.vn>
2022-04-25 10:32:41 +0700

    health

--- SQL Scripts/HEALTH.sql
+++ SQL Scripts/HEALTH.sql
@@ -6,10 +6,14 @@
 
 create table HEALTH
 (
+<<<<<<< Updated upstream
+=======
+      ID int,
+>>>>>>> Stashed changes
 	PersonalID varchar2(12),
 	FilledDate date,
 	Healths varchar2(20),
-	Note varchar2(2000)
+	Note varchar2(2000),
 );
 
 /*	CONSTRAINT	*/
@@ -30,9 +34,10 @@ add constraint FK_HEAL_PERSON foreign key (PersonalID) references PERSON(ID)
 ---check Filleddate=SYSDATE
 
 create or replace trigger HEALTH_FilledDate
-after insert on HEALTH
+after insert or update on HEALTH
 for each row
 begin
+<<<<<<< Updated upstream
 if((Extract(Year From : new.FilledDate) = Extract(Year From SYSDATE))
 and (Extract(Month From : new.FilledDate) = Extract(Year From SYSDATE))
 and (Extract(Year From : new.FilledDate) = Extract(Year From SYSDATE))
@@ -40,8 +45,15 @@ then
 DBMS_OUTPUT.PUT_LINE('Successfully added filled date');
 else
 RAISE_APPLICATION_ERROR(-10000,'Filled date must be according to SYSDATE!);
+=======
+    if((Extract(Day From : new.FilledDate) != Extract(Day From SYSDATE))
+    and (Extract(Month From : new.FilledDate) != Extract(Month From SYSDATE))
+    and (Extract(Year From : new.FilledDate) != Extract(Year From SYSDATE))
+    then 
+        RAISE_APPLICATION_ERROR(-10000,'Filled date must be according to SYSDATE!);
+>>>>>>> Stashed changes
 end if;
-end;
+end HEALTH_FilledDate;
 
 
 /*	STORED PROCEDURES	*/
@@ -49,6 +61,21 @@ end;
 
 
 /*	STORED FUNCTIONS	*/
+--return the next ID for filling form
+
+create or replace function NUM__COUNT_HEAL_FORM_ID(par_personalID PERSON.ID%type)
+return number is
+--return value of count health form
+Count_HEAL_FORM int;
+begin
+if(par_personalID = NULL)
+then 
+return NULL;
+end if;
+select Count(PersonalID) into Count_HEAL_FORM
+from HEALTH
+return Count_HEAL_FORM;
+end NUM_COUNT_HEAL_FORM_ID;
 
 
 
