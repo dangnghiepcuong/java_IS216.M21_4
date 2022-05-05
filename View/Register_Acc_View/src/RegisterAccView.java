35c1fc0e9ee8053d221776be98a76d74cf6a472e
lychthac <ccdl2002@gmail.com>
2022-05-05 21:41:12 +0700

    RegAcc

--- View/Register_Acc_View/src/RegisterAccView.java
+++ View/Register_Acc_View/src/RegisterAccView.java
@@ -38,14 +38,24 @@ public class RegisterAccView extends JFrame implements ActionListener
     private JLabel UsernameLabel;
     private JLabel PasswordLabel;
     private JLabel RepeatPasswordLabel;
-
     private JLabel LastNameLabel;
-
     private JLabel FirstNameLabel;
-
     private JLabel IDLabel;
-
     private JLabel BirthdayLabel;
+
+    private JLabel GenderLabel;
+
+    private JLabel HomeTownLabel;
+
+    private JLabel ProvinceLabel;
+
+    private JLabel DistrictLabel;
+
+    private JLabel TownLabel;
+
+    private JLabel StreetLabel;
+
+    private JLabel EmailLabel;
     private JTextField UsernameTextField;
     private JPasswordField PasswordField;
     private JPasswordField RepeatPasswordField;
@@ -56,8 +66,21 @@ public class RegisterAccView extends JFrame implements ActionListener
 
     private JTextField IDTextField;
 
+    private JTextField StreetTextField;
+
+    private JTextField EmailTextField;
+
     private JButton RegisterAccButton;
 
+    private Choice GenderChoice;
+
+    private Choice HomeTownChoice;
+
+    private Choice ProvinceChoice;
+
+    private Choice DistrictChoice;
+
+    private Choice TownChoice;
     private JDatePickerImpl BirthdayField;
 
     private JPanel AccInfoPanel;
@@ -147,7 +170,47 @@ public class RegisterAccView extends JFrame implements ActionListener
         initBirthdayField();
         PersonalInfoPanel.add(BirthdayField);
 
-        //PersonalInfoPanel.getContentPanel().add(BirthdayField);
+        initGenderLabel();
+        PersonalInfoPanel.add(GenderLabel);
+
+        initGenderChoice();
+        PersonalInfoPanel.add(GenderChoice);
+
+        initHomeTownLabel();
+        PersonalInfoPanel.add(HomeTownLabel);
+
+        initHomeTownChoice();
+        PersonalInfoPanel.add(HomeTownChoice);
+
+        initProvinceLabel();
+        PersonalInfoPanel.add(ProvinceLabel);
+
+        initProvinceChoice();
+        PersonalInfoPanel.add(ProvinceChoice);
+
+        initDistrictLabel();
+        PersonalInfoPanel.add(DistrictLabel);
+
+        initDistrictChoice();
+        PersonalInfoPanel.add(DistrictChoice);
+
+        initTownLabel();
+        PersonalInfoPanel.add(TownLabel);
+
+        initTownChoice();
+        PersonalInfoPanel.add(TownChoice);
+
+        initStreetLabel();
+        PersonalInfoPanel.add(StreetLabel);
+
+        initStreetTextField();
+        PersonalInfoPanel.add(StreetTextField);
+
+        initEmailLabel();
+        PersonalInfoPanel.add(EmailLabel);
+
+        initEmailTextField();
+        PersonalInfoPanel.add(EmailTextField);
 
         PersonalInfoPanel.validate();
     }
@@ -169,7 +232,7 @@ public class RegisterAccView extends JFrame implements ActionListener
         //set label text style
         UsernameLabel.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
 
-//        //set label text color
+        //set label text color
         UsernameLabel.setForeground(new Color(0x666666));
 
         UsernameLabel.setHorizontalAlignment(JLabel.LEFT);
@@ -361,7 +424,7 @@ public class RegisterAccView extends JFrame implements ActionListener
         FirstNameLabel = new JLabel();
 
         //set position and area
-        FirstNameLabel.setBounds(70+220+50, 80, 240, 30);
+        FirstNameLabel.setBounds(70+220+25, 80, 240, 30);
 
         //set label text
         FirstNameLabel.setText("Tên");
@@ -382,7 +445,7 @@ public class RegisterAccView extends JFrame implements ActionListener
         FirstNameTextField = new JTextField();
 
         //set position and area
-        FirstNameTextField.setBounds(70+220+50, 80 + DefaultLabelHeigth(), 150, 30);
+        FirstNameTextField.setBounds(70+220+25, 80 + DefaultLabelHeigth(), 150, 30);
 
         //set cursor
         FirstNameTextField.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
@@ -488,23 +551,321 @@ public class RegisterAccView extends JFrame implements ActionListener
     }
     private void initBirthdayField()
     {
-        SqlDateModel model = new SqlDateModel();
+        UtilDateModel model=new UtilDateModel();
         Properties p = new Properties();
-        p.put("text.today", "Day");
-        p.put("text.month", "Month");
-        p.put("text.year", "Year");
-
         JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
-        BirthdayField = new JDatePickerImpl(datePanel, new AbstractFo);
+        BirthdayField = new JDatePickerImpl(datePanel, new DateLabelFormatter());
 
-        BirthdayField.setBounds(70, 100 + 3*DefaultLabelHeigth()+2*DefaultFieldHeigth(),220,30);
+        BirthdayField.setBounds(70, 100 + 3*DefaultLabelHeigth()+2*DefaultFieldHeigth(),170,30);
 
-        BirthdayField.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+        JFormattedTextField textField = BirthdayField.getJFormattedTextField();
+        textField.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+        textField.setBounds(70, 100 + 3*DefaultLabelHeigth()+2*DefaultFieldHeigth(),170,30);
+        textField.setBackground(Color.WHITE);
 
         BirthdayField.setForeground(new Color(dv.BlackTextColor()));
+    }
+    private void initGenderLabel()
+    {
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
 
+        //create new label
+        GenderLabel = new JLabel();
+
+        //set label position and frame area
+        GenderLabel.setBounds(70 + 25 + 220, 100 + 2*DefaultLabelHeigth()+2*DefaultFieldHeigth(), 240, 30);
+        GenderLabel.setPreferredSize(new Dimension(240,30));
+
+        //set label text
+        GenderLabel.setText("Giới tính");
+
+        //set label text style
+        GenderLabel.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        //set label text color
+        GenderLabel.setForeground(new Color(0x666666));
+
+        GenderLabel.setHorizontalAlignment(JLabel.LEFT);
+        GenderLabel.setVerticalAlignment(JLabel.CENTER);
+    }
+    private void initGenderChoice()
+    {
+        GenderChoice = new Choice();
+
+        GenderChoice.setBounds(70 + 25 + 220, 100 + 3*DefaultLabelHeigth()+2*DefaultFieldHeigth(), 80, 30);
+
+        GenderChoice.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        GenderChoice.setForeground(new Color(0x666666));
+
+        GenderChoice.setBackground(Color.WHITE);
+
+        GenderChoice.add("Nữ");
+        GenderChoice.add("Nam");
+        GenderChoice.add("Khác");
     }
 
+    private void initHomeTownLabel()
+    {
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+
+        //create new label
+        HomeTownLabel = new JLabel();
+
+        //set label position and frame area
+        HomeTownLabel.setBounds(70, 110 + 3*DefaultLabelHeigth()+3*DefaultFieldHeigth(), 240, 30);
+        HomeTownLabel.setPreferredSize(new Dimension(240,30));
+
+        //set label text
+        HomeTownLabel.setText("Quê quán");
+
+        //set label text style
+        HomeTownLabel.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        //set label text color
+        HomeTownLabel.setForeground(new Color(0x666666));
+
+        HomeTownLabel.setHorizontalAlignment(JLabel.LEFT);
+        HomeTownLabel.setVerticalAlignment(JLabel.CENTER);
+    }
+
+    private void initHomeTownChoice()
+    {
+        HomeTownChoice = new Choice();
+
+        HomeTownChoice.setBounds(70, 110 + 4*DefaultLabelHeigth()+3*DefaultFieldHeigth(), 170, 30);
+
+        HomeTownChoice.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        HomeTownChoice.setForeground(new Color(0x666666));
+
+        HomeTownChoice.setBackground(Color.WHITE);
+
+        HomeTownChoice.add("Hồ Chí Minh");
+        HomeTownChoice.add("Bình Dương");
+        HomeTownChoice.add("Hà Nội");
+    }
+
+    private void initProvinceLabel()
+    {
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+
+        //create new label
+        ProvinceLabel = new JLabel();
+
+        //set label position and frame area
+        ProvinceLabel.setBounds(70, 120 + 4*DefaultLabelHeigth()+4*DefaultFieldHeigth(), 240, 30);
+        ProvinceLabel.setPreferredSize(new Dimension(240,30));
+
+        //set label text
+        ProvinceLabel.setText("Tỉnh/thành phố cư trú");
+
+        //set label text style
+        ProvinceLabel.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        //set label text color
+        ProvinceLabel.setForeground(new Color(0x666666));
+
+        ProvinceLabel.setHorizontalAlignment(JLabel.LEFT);
+        ProvinceLabel.setVerticalAlignment(JLabel.CENTER);
+    }
+
+    private void initProvinceChoice()
+    {
+        ProvinceChoice = new Choice();
+
+        ProvinceChoice.setBounds(70, 120 + 5*DefaultLabelHeigth()+4*DefaultFieldHeigth(), 170, 30);
+
+        ProvinceChoice.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        ProvinceChoice.setForeground(new Color(0x666666));
+
+        ProvinceChoice.setBackground(Color.WHITE);
+
+        ProvinceChoice.add("Hồ Chí Minh");
+        ProvinceChoice.add("Đồng Nai");
+        ProvinceChoice.add("Bình Dương");
+        ProvinceChoice.add("Hà Nội");
+    }
+
+    private void initDistrictLabel()
+    {
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+
+        //create new label
+        DistrictLabel = new JLabel();
+
+        //set label position and frame area
+        DistrictLabel.setBounds(70 + 25 +170, 120 + 4*DefaultLabelHeigth()+4*DefaultFieldHeigth(), 240, 30);
+        DistrictLabel.setPreferredSize(new Dimension(240,30));
+
+        //set label text
+        DistrictLabel.setText("Quận/huyện cư trú");
+
+        //set label text style
+        DistrictLabel.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        //set label text color
+        DistrictLabel.setForeground(new Color(0x666666));
+
+        DistrictLabel.setHorizontalAlignment(JLabel.LEFT);
+        DistrictLabel.setVerticalAlignment(JLabel.CENTER);
+    }
+
+    private void initDistrictChoice()
+    {
+        DistrictChoice = new Choice();
+
+        DistrictChoice.setBounds(70+25+170, 120 + 5*DefaultLabelHeigth()+4*DefaultFieldHeigth(), 170, 30);
+
+        DistrictChoice.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        DistrictChoice.setForeground(new Color(0x666666));
+
+        DistrictChoice.setBackground(Color.WHITE);
+
+        DistrictChoice.add("Thủ Đức");
+        DistrictChoice.add("Biên Hòa");
+        DistrictChoice.add("Cẩm Mỹ");
+        DistrictChoice.add("Thủ Dầu Một");
+    }
+
+    private void initTownLabel()
+    {
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+
+        //create new label
+        TownLabel = new JLabel();
+
+        //set label position and frame area
+        TownLabel.setBounds(70 + 50 +2*170, 120 + 4*DefaultLabelHeigth()+4*DefaultFieldHeigth(), 240, 30);
+        TownLabel.setPreferredSize(new Dimension(240,30));
+
+        //set label text
+        TownLabel.setText("Phường/xã cư trú");
+
+        //set label text style
+        TownLabel.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        //set label text color
+        TownLabel.setForeground(new Color(0x666666));
+
+        TownLabel.setHorizontalAlignment(JLabel.LEFT);
+        TownLabel.setVerticalAlignment(JLabel.CENTER);
+    }
+
+    private void initTownChoice()
+    {
+        TownChoice = new Choice();
+
+        TownChoice.setBounds(70+50+2*170, 120 + 5*DefaultLabelHeigth()+4*DefaultFieldHeigth(), 170, 30);
+
+        TownChoice.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        TownChoice.setForeground(new Color(0x666666));
+
+        TownChoice.setBackground(Color.WHITE);
+
+        TownChoice.add("Linh Trung");
+        TownChoice.add("Dầu Tiếng");
+        TownChoice.add("Lái Thiêu");
+        TownChoice.add("Tân Hòa");
+        TownChoice.add("Sông Ray");
+    }
+
+    private void initStreetLabel()
+    {
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+
+        //create new label
+        StreetLabel = new JLabel();
+
+        //set label position and frame area
+        StreetLabel.setBounds(70, 130 + 5*DefaultLabelHeigth()+5*DefaultFieldHeigth(), 240, 30);
+        StreetLabel.setPreferredSize(new Dimension(240,30));
+
+        //set label text
+        StreetLabel.setText("Số nhà, tên đường, khu phố/ấp");
+
+        //set label text style
+        StreetLabel.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        //set label text color
+        StreetLabel.setForeground(new Color(0x666666));
+
+        StreetLabel.setHorizontalAlignment(JLabel.LEFT);
+        StreetLabel.setVerticalAlignment(JLabel.CENTER);
+    }
+
+    private void initStreetTextField()
+    {
+        //create
+        StreetTextField = new JTextField();
+
+        //set position and area
+        StreetTextField.setBounds(70, 130 + 6*DefaultLabelHeigth()+5*DefaultFieldHeigth(), 300, 30);
+
+        //set cursor
+        StreetTextField.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
+
+        //set field font
+        StreetTextField.setFont(new Font("SVN-Arial", Font.PLAIN, DefaultLabelFontSize()));
+
+        //set text field color
+        StreetTextField.setForeground(new Color(0x333333));
+
+        //set field background color
+        StreetTextField.setBackground(Color.WHITE);
+
+        StreetTextField.setPreferredSize(new java.awt.Dimension(220, 30));
+    }
+
+    private void initEmailLabel()
+    {
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+
+        //create new label
+        EmailLabel = new JLabel();
+
+        //set label position and frame area
+        EmailLabel.setBounds(70, 140 + 6*DefaultLabelHeigth()+6*DefaultFieldHeigth(), 240, 30);
+        EmailLabel.setPreferredSize(new Dimension(240,30));
+
+        //set label text
+        EmailLabel.setText("Email");
+
+        //set label text style
+        EmailLabel.setFont(new Font("SVN-Arial", 0, DefaultLabelFontSize()));
+
+        //set label text color
+        EmailLabel.setForeground(new Color(0x666666));
+
+        EmailLabel.setHorizontalAlignment(JLabel.LEFT);
+        EmailLabel.setVerticalAlignment(JLabel.CENTER);
+    }
+
+    private void initEmailTextField()
+    {
+        //create
+        EmailTextField = new JTextField();
+
+        //set position and area
+        EmailTextField.setBounds(70, 140 + 7*DefaultLabelHeigth()+6*DefaultFieldHeigth(), 220, 30);
+
+        //set cursor
+        EmailTextField.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
+
+        //set field font
+        EmailTextField.setFont(new Font("SVN-Arial", Font.PLAIN, DefaultLabelFontSize()));
+
+        //set text field color
+        EmailTextField.setForeground(new Color(0x333333));
+
+        //set field background color
+        EmailTextField.setBackground(Color.WHITE);
+
+        EmailTextField.setPreferredSize(new java.awt.Dimension(220, 30));
+    }
     private void initFrameComponent()
     {
         //Frame
@@ -541,7 +902,6 @@ public class RegisterAccView extends JFrame implements ActionListener
         initPersonalInfoPanel();
         this.add(PersonalInfoPanel);
     }
-
     public RegisterAccView()
     {
         initFrameComponent();
