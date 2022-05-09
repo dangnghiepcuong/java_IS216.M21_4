de1890bf7f99e66d1b190ece5b8098121e79590f
lychthac <ccdl2002@gmail.com>
2022-05-09 15:07:27 +0700

    userinfo

--- View/VACCINATION_MANAGEMENT/src/GUI_Main/CitizenMainView.java
+++ View/VACCINATION_MANAGEMENT/src/GUI_Main/CitizenMainView.java
@@ -478,7 +478,7 @@ public class CitizenMainView extends JFrame implements ActionListener
         {
             userInformationView = null;
             MainLayeredPane.removeAll();
-            CitizenMainView citizenMainView = new CitizenMainView(personalUser.getPhone());
+            CitizenMainView citizenMainView = new CitizenMainView(userInformationView.getPersonalUser().getPhone());
             this.dispose();
         }
 
@@ -499,11 +499,13 @@ public class CitizenMainView extends JFrame implements ActionListener
 
         if (e.getSource() == InfoSettingButton)
         {
-            userInformationView = new UserInformationView();
+            userInformationView = new UserInformationView(personalUser);
             MainLayeredPane.add(userInformationView, Integer.valueOf(1));
             MainLayeredPane.repaint(0,0,dv.FrameWidth(), dv.FrameHeight());
             initBackInfoButton();
             MainLayeredPane.add(BackInfoButton, Integer.valueOf(5));
+
+            //personalUser = userInformationView.getPersonalUser();
         }
 
         if (e.getSource() == SearchButton)
